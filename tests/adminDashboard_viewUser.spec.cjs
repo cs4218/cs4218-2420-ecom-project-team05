c2fb4520bfc466fe9397933037687102b8a0a62e
ThomasHoooo <e0725843@u.nus.edu>
2025-03-18 13:54:02 +0800

    edit description

--- tests/adminDashboard_viewUser.spec.cjs
+++ tests/adminDashboard_viewUser.spec.cjs
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('test admin dashboard view users', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await page.getByRole('link', { name: 'Login' }).click();
   await page.getByRole('textbox', { name: 'Enter Your Email' }).click();
